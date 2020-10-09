1dbe9be201640df77c8e5d55734fa1cd7b3d9403
XvFeng <gjh4961@qq.com>
2020-10-09 11:32:24 -0400

    'clear stage' and add new file test2.txt in week1/note

--- week2/code/2_1.c
+++
@@ -1,8 +0,0 @@
-#include "myhead.h"
-
-void main(void)
-{
-	int ret = write(1, "Hello World! \n", 14);
-
-	exit(0);
-}
--- week2/code/myhead.h
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<fcntl.h>
-#include<sys/stat.h>
-#include<string.h>
-
